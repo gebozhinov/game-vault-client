6f6b2faa735eb432617927c5e5ad45e1eb3f13ea
Georgi Bozhinov <ge.bozhinov@gmail.com>
2026-06-13 20:29:08 +0300

    extract the index html into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer";
+import Home from "./components/home/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+
+      <Home />
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
